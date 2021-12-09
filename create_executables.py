0138808a7f5f2cdb561a5a1cdc6ca81460ba0fe7
aditya9706 <41884965+aditya9706@users.noreply.github.com>
2021-12-09 19:05:18 +0530

    Create create_executables.py

---
+++ create_executables.py
@@ -0,0 +1,28 @@
+import glob
+import os 
+
+# creating object files 
+list_cpp = glob.glob("*.cpp")
+print(list_cpp)
+for cpp in list_cpp :
+  file_name = cpp.split(".")[0]
+  os.system("g++ -I/usr/local/cuda/include -I/usr/local/cuda/targets/ppc64le-linux/include -o {file}.o -c {cpp_file}".format(file = file_name, cpp_file = cpp))
+
+# creating executables
+list_obj = glob.glob("*.o")
+print(list_obj)
+os.system("mkdir Executables")
+for obj in list_obj :
+  file_name = obj.split(".")[0]
+  os.system("/usr/local/cuda/bin/nvcc -ccbin g++ -m64 -gencode arch=compute_70,code=sm_70 -o Executables/{executable} {object}.o -I/usr/local/cuda/include 
+             -I/usr/local/cuda/targets/ppc64le-linux/include-L/usr/local/cuda/lib64 -L/usr/local/cuda/targets/ppc64le-linux/lib -lcublas -lcudnn 
+             -lstdc++ -lm".format(executable = file_name, object = obj))
+
+# Reading config file      
+config = open(args["-testfile"], "r")
+           
+# Running executables            
+for cmd in config:
+  print(cmd)         
+  os.system(cmd)          
+  
