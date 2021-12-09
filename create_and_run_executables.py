6a8404ddd26e11583a46611f65b0d8a069a2b1ce
aditya9706 <41884965+aditya9706@users.noreply.github.com>
2021-12-09 19:42:28 +0530

    Update create_and_run_executables.py

--- create_and_run_executables.py
+++ create_and_run_executables.py
@@ -15,7 +15,7 @@ print(list_obj)
 os.system("mkdir Executables")
 for obj in list_obj :
   file_name = obj.split(".")[0]
-  os.system("/usr/local/cuda/bin/nvcc -ccbin g++ -m64 -gencode arch=compute_70,code=sm_70 -o Executables/{executable} {object}.o -I/usr/local/cuda/include -I/usr/local/cuda/targets/ppc64le-linux/include-L/usr/local/cuda/lib64 -L/usr/local/cuda/targets/ppc64le-linux/lib -lcublas -lcudnn -lstdc++ -lm".format(executable = file_name, object = obj))
+  os.system("/usr/local/cuda/bin/nvcc -ccbin g++ -m64 -gencode arch=compute_70,code=sm_70 -o Executables/{executable} {object} -I/usr/local/cuda/include -I/usr/local/cuda/targets/ppc64le-linux/include-L/usr/local/cuda/lib64 -L/usr/local/cuda/targets/ppc64le-linux/lib -lcublas -lcudnn -lstdc++ -lm".format(executable = file_name, object = obj))
 
 # Reading config file     
 ap = argparse.ArgumentParser()
