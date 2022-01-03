bee4d943753356fa9bcbfbde2e85cc6ca8331faf
aditya9706 <41884965+aditya9706@users.noreply.github.com>
2022-01-03 17:55:07 +0530

    Update cublas_gemm_test.cpp

--- Latest/cublas_gemm_test.cpp
+++ Latest/cublas_gemm_test.cpp
@@ -2,18 +2,7 @@
 #include <string>
 #include "cublas.h"
 #include "cublas_v2.h"
-
-#define FIRST_ARG "x_row"    // name for first command line argument
-#define SECOND_ARG "x_col"    // name for second command line argument
-#define THIRD_ARG "y_col"   // name for third command line argument
-#define FOURTH_ARG "alpha"    // name for fourth command line argument
-#define FIFTH_ARG "beta"    // name for fifth command line argument
-#define FIRST_ARG_LEN 5      // length of first command line argument
-#define SECOND_ARG_LEN 5     // length of second command line argument
-#define THIRD_ARG_LEN 5      // length of third command line argument
-#define FOURTH_ARG_LEN 5      // length of fourth command line argument
-#define FIFTH_ARG_LEN 4     // length of fifth command line argument
-#define BEGIN 1              
+           
 #define INDEX(row, col, row_count) (((col) * (row_count)) + (row))    // for getting index values matrices
 #define RANDOM (rand() % 10000 * 1.00) / 100    // to generate random values 
 
@@ -34,36 +23,39 @@ void PrintMatrix(float* Matrix, int matrix_row, int matrix_col) {
 
 int main (int argc, char **argv) {
   
-  int x_row, x_col, y_row, y_col, z_row, z_col;
+  int A_row, A_col, B_row, B_col, C_row, C_col;
   float alpha, beta;
 
-  std::cout << std::endl;
-  for (int loop_count = 0; loop_count < argc; loop_count++) {
-    std::cout << argv[loop_count] << std::endl;
+  std::cout << argv[0] << std::endl;
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::cout << argv[loop_count] << " ";
+    if(loop_count + 1 < argc)
+      std::cout << argv[loop_count + 1] << std::endl;
   }
+  std::cout << std::endl;
 
   // reading cmd line arguments
-  for (int loop_count = 1; loop_count < argc; loop_count++) {
-           std::string str(argv[loop_count]);  
-    if (!((str.substr(BEGIN, FIRST_ARG_LEN)).compare(FIRST_ARG)))
-      x_row = atoi(argv[loop_count] + FIRST_ARG_LEN + 1);
+  for (int loop_count = 1; loop_count < argc; loop_count += 2) {
+    std::string cmd_argument(argv[loop_count]);  
+    if (!(cmd_argument.compare("-A_row")))
+      A_row = atoi(argv[loop_count + 1]);
       
-    else if (!((str.substr(BEGIN, SECOND_ARG_LEN)).compare(SECOND_ARG)))
-      x_col = atoi(argv[loop_count] + SECOND_ARG_LEN + 1);
+    else if (!(cmd_argument.compare("-A_column")))
+      A_col = atoi(argv[loop_count + 1]);
 
-    else if (!((str.substr(BEGIN, THIRD_ARG_LEN)).compare(THIRD_ARG)))
-      y_col = atoi(argv[loop_count] + THIRD_ARG_LEN + 1);
+    else if (!(cmd_argument.compare("-B_column")))
+      B_col = atoi(argv[loop_count + 1]);
 
-    else if (!((str.substr(BEGIN, FOURTH_ARG_LEN)).compare(FOURTH_ARG)))
-      alpha = atof(argv[loop_count] + FOURTH_ARG_LEN + 1);
+    else if (!(cmd_argument.compare("-alpha")))
+      alpha = atof(argv[loop_count + 1]);
 
-    else if (!((str.substr(BEGIN, FIFTH_ARG_LEN)).compare(FIFTH_ARG)))
-      beta = atof(argv[loop_count] + FIFTH_ARG_LEN + 1);
+    else if (!(cmd_argument.compare("-beta")))
+      beta = atof(argv[loop_count + 1]);
   }
  
-  y_row = x_col;
-  z_row = x_row;
-  z_col = y_col;
+  B_row = A_col;
+  C_row = A_row;
+  C_col = B_col;
   
   cudaError_t cudaStatus; 
   cublasStatus_t status; 
@@ -71,81 +63,81 @@ int main (int argc, char **argv) {
 
   clock_t clk_start, clk_end;   
  
-  float *HostMatX; // mxk matrix x on the host
-  float *HostMatY; // kxn matrix y on the host
-  float *HostMatZ; // mxn matrix z on the host
+  float *HostMatA; // mxk matrix A on the host
+  float *HostMatB; // kxn matrix B on the host
+  float *HostMatC; // mxn matrix C on the host
   
-  HostMatX = new float[x_row * x_col]; // host memory for x
-  HostMatY = new float[y_row * y_col]; // host memory for y
-  HostMatZ = new float[z_row * z_col]; // host memory for z
+  HostMatA = new float[A_row * A_col]; // host memory for A
+  HostMatB = new float[B_row * B_col]; // host memory for B
+  HostMatC = new float[C_row * C_col]; // host memory for C
   
-  if (HostMatX == 0) {
-    fprintf (stderr, "!!!! Host memory allocation error (matrixX)\n");
+  if (HostMatA == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixA)\n");
     return EXIT_FAILURE;
   }
-  if (HostMatY == 0) {
-    fprintf (stderr, "!!!! Host memory allocation error (matrixY)\n");
+  if (HostMatB == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixB)\n");
     return EXIT_FAILURE;
   }
-  if (HostMatZ == 0) {
-    fprintf (stderr, "!!!! Host memory allocation error (matrixZ)\n");
+  if (HostMatC == 0) {
+    fprintf (stderr, "!!!! Host memory allocation error (matrixC)\n");
     return EXIT_FAILURE;
   }
 
   int row, col;
   
-  // define an mxk matrix x column by column
+  // define an mxk matrix A column by column
   // using RANDOM macro to generate random numbers between 0 - 100
-  for (row = 0; row < x_row; row++) {                                              
-    for (col = 0; col < x_col; col++) {                                                   
-      HostMatX[INDEX(row, col, x_row)] = RANDOM;                                      
+  for (row = 0; row < A_row; row++) {                                              
+    for (col = 0; col < A_col; col++) {                                                   
+      HostMatA[INDEX(row, col, A_row)] = RANDOM;                                      
     }                                                                                    
   }                                                                               
                                                                                
-  // define a kxn matrix y column by column
+  // define a kxn matrix B column by column
   // using RANDOM macro to generate random numbers between 0 - 100
-  for (row = 0; row < y_row; row++) {                                      
-    for (col = 0; col < y_col; col++) {                                                
-      HostMatY[INDEX(row, col, y_row)] = RANDOM;                                           
+  for (row = 0; row < B_row; row++) {                                      
+    for (col = 0; col < B_col; col++) {                                                
+      HostMatB[INDEX(row, col, B_row)] = RANDOM;                                           
     }                                                                         
   }                                                       
   
-  // define an mxn matrix z column by column
+  // define an mxn matrix C column by column
   // using RANDOM macro to generate random numbers between 0 - 100
-  for (row = 0; row < z_row; row++) {                             
-    for (col = 0; col < z_col; col++) {                                        
-      HostMatZ[INDEX(row, col, z_row)] = RANDOM;                 
+  for (row = 0; row < C_row; row++) {                             
+    for (col = 0; col < C_col; col++) {                                        
+      HostMatC[INDEX(row, col, C_row)] = RANDOM;                 
     }                                                                  
   }
   
   // printing input matrices
-  std::cout << "\nMatrix X:\n";
-  PrintMatrix(HostMatX, x_row, x_col);
-  std::cout << "\nMatrix Y:\n";
-  PrintMatrix(HostMatY, y_row, y_col);
-  std::cout << "\nMatrix Z:\n";
-  PrintMatrix(HostMatZ, z_row, z_col);                                                           
+  std::cout << "\nMatrix A:\n";
+  PrintMatrix(HostMatA, A_row, A_col);
+  std::cout << "\nMatrix B:\n";
+  PrintMatrix(HostMatB, B_row, B_col);
+  std::cout << "\nMatrix C:\n";
+  PrintMatrix(HostMatC, C_row, C_col);                                                           
   
   // on the device
-  float *DeviceMatX; // d_x - x on the device
-  float *DeviceMatY; // d_y - y on the device
-  float *DeviceMatZ; // d_z - z on the device
+  float *DeviceMatA; // d_A - A on the device
+  float *DeviceMatB; // d_B - B on the device
+  float *DeviceMatC; // d_C - C on the device
 
-  cudaStatus = cudaMalloc ((void **) &DeviceMatX , x_row * x_col * sizeof (*HostMatX));
+  cudaStatus = cudaMalloc ((void **) &DeviceMatA , A_row * A_col * sizeof (*HostMatA));
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory allocation failed for X " << std::endl;
+    std::cout << " The device memory allocation failed for A " << std::endl;
     return EXIT_FAILURE;
   }
 
-  cudaStatus = cudaMalloc ((void **) &DeviceMatY , y_row * y_col * sizeof (*HostMatY));
+  cudaStatus = cudaMalloc ((void **) &DeviceMatB , B_row * B_col * sizeof (*HostMatB));
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory allocation failed for Y " << std::endl;
+    std::cout << " The device memory allocation failed for B " << std::endl;
     return EXIT_FAILURE;
   }
 
-  cudaStatus = cudaMalloc ((void **) &DeviceMatZ , z_row * z_col * sizeof (*HostMatZ));
+  cudaStatus = cudaMalloc ((void **) &DeviceMatC , C_row * C_col * sizeof (*HostMatC));
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory allocation failed for Z " << std::endl;
+    std::cout << " The device memory allocation failed for C " << std::endl;
     return EXIT_FAILURE;   
   }
   
@@ -156,32 +148,32 @@ int main (int argc, char **argv) {
   }
 
   // copy matrices from the host to the device
-  status = cublasSetMatrix (x_row, x_col, sizeof (*HostMatX), HostMatX, x_row, DeviceMatX, x_row); // x -> d_x
+  status = cublasSetMatrix (A_row, A_col, sizeof (*HostMatA), HostMatA, A_row, DeviceMatA, A_row); // A -> d_A
   if (status != CUBLAS_STATUS_SUCCESS) {
-    fprintf (stderr, "Copying matrix X from host to device failed \n");
+    fprintf (stderr, "Copying matrix A from host to device failed \n");
     return EXIT_FAILURE;
   }
   
-  status = cublasSetMatrix (y_row, y_col, sizeof (*HostMatY), HostMatY, y_row, DeviceMatY, y_row); // y -> d_y
+  status = cublasSetMatrix (B_row, B_col, sizeof (*HostMatB), HostMatB, B_row, DeviceMatB, B_row); // B -> d_B
   if (status != CUBLAS_STATUS_SUCCESS) {
-    fprintf (stderr, "Copying matrix Y from host to device failed\n");
+    fprintf (stderr, "Copying matrix B from host to device failed\n");
     return EXIT_FAILURE;
   }
-  status = cublasSetMatrix (z_row, z_col, sizeof (*HostMatZ), HostMatZ, z_row, DeviceMatZ, z_row); // z -> d_z
+  status = cublasSetMatrix (C_row, C_col, sizeof (*HostMatC), HostMatC, C_row, DeviceMatC, C_row); // C -> d_C
   if (status != CUBLAS_STATUS_SUCCESS) {
-    fprintf (stderr, "Copying matrix Z from host to device failed\n");
+    fprintf (stderr, "Copying matrix C from host to device failed\n");
     return EXIT_FAILURE;
   }
   
 
   clk_start = clock();
 
-  // matrix - matrix multiplication : d_z = alpha * d_x * d_y + beta * d_z
-  // d_x -mxk matrix , d_y -kxn matrix , d_z -mxn matrix
+  // matrix - matrix multiplication : d_C = alpha * d_A * d_B + beta * d_C
+  // d_A -mxk matrix , d_B -kxn matrix , d_C -mxn matrix
   // alpha, beta - scalars
-  status = cublasSgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, x_row, 
-                        y_col, x_col, &alpha, DeviceMatX, x_row,
-                        DeviceMatY, y_row, &beta, DeviceMatZ, z_row);
+  status = cublasSgemm (handle, CUBLAS_OP_N, CUBLAS_OP_N, A_row, 
+                        B_col, A_col, &alpha, DeviceMatA, A_row,
+                        DeviceMatB, B_row, &beta, DeviceMatC, C_row);
   
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf (stderr, "!!!! kernel execution error\n");
@@ -190,36 +182,36 @@ int main (int argc, char **argv) {
 
   clk_end = clock();
   
-  status = cublasGetMatrix (z_row, z_col, sizeof (*HostMatZ),
-                            DeviceMatZ, z_row, HostMatZ, z_row); // copy d_z -> z
+  status = cublasGetMatrix (C_row, C_col, sizeof (*HostMatC),
+                            DeviceMatC, C_row, HostMatC, C_row); // copy d_z -> C
 
   if (status != CUBLAS_STATUS_SUCCESS) {
-    fprintf (stderr, "!!!! Unable to get output matrix Z from device\n");
+    fprintf (stderr, "!!!! Unable to get output matrix C from device\n");
     return EXIT_FAILURE;
   }
   
-  std::cout << "\nMatriz Z after Gemm operation is:\n";
-  PrintMatrix(HostMatZ, z_row, z_col); 
+  std::cout << "\nMatriz C after Gemm operation is:\n";
+  PrintMatrix(HostMatC, C_row, C_col); 
   
   // printing latency and throughput of the function
   std::cout << "\nLatency: " <<  ((double)(clk_end - clk_start)) / double(CLOCKS_PER_SEC) <<
                "\nThroughput: " <<THROUGHPUT(clk_start, clk_end) << "\n\n";
   
-  cudaStatus = cudaFree (DeviceMatX); // free device memory
+  cudaStatus = cudaFree (DeviceMatA); // free device memory
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory deallocation failed for X" << std::endl;
+    std::cout << " The device memory deallocation failed for A" << std::endl;
     return EXIT_FAILURE;   
   }
   
-  cudaStatus = cudaFree (DeviceMatY); // free device memory
+  cudaStatus = cudaFree (DeviceMatB); // free device memory
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory deallocation failed for Y" << std::endl;
+    std::cout << " The device memory deallocation failed for B" << std::endl;
     return EXIT_FAILURE;   
   }
   
-  cudaStatus = cudaFree (DeviceMatZ); // free device memory
+  cudaStatus = cudaFree (DeviceMatC); // free device memory
   if( cudaStatus != cudaSuccess) {
-    std::cout << " The device memory deallocation failed for Z" << std::endl;
+    std::cout << " The device memory deallocation failed for C" << std::endl;
     return EXIT_FAILURE;   
   }
   
@@ -229,9 +221,9 @@ int main (int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  delete[] HostMatX; // free host memory
-  delete[] HostMatY; // free host memory
-  delete[] HostMatZ; // free host memory
+  delete[] HostMatA; // free host memory
+  delete[] HostMatB; // free host memory
+  delete[] HostMatC; // free host memory
 
   return EXIT_SUCCESS ;
 }
